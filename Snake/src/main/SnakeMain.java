8965581a455c189994e04234dbd903f8053b577b
raabdo <rachidabderrahman5@gmail.com>
2022-11-22 03:55:46 +0800

    Add files via upload

---
+++ Snake/src/main/SnakeMain.java
@@ -0,0 +1,12 @@
+package main;
+
+import components.GameFrame;
+
+public class SnakeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GameFrame(600, 800, 15);		
+	}
+
+}
